192c9ff3786fe411022a3938d19ff784635cc8c1
DeibidSE <davidsimon98@outlook.com>
2023-08-27 20:29:29 +0200

    Updates

---
+++ assets/config.f924af71.js
@@ -0,0 +1,1 @@
+import{R as p,S as o,u as i}from"./entry.b5e98c60.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
---
+++ assets/default.4449cba4.js
@@ -0,0 +1,1 @@
+import{_ as e,l as r,G as o,k as a}from"./entry.b5e98c60.js";const s={},n={class:"bg-gradient-to-r from-purple-600 to-pink-500 text-dark dark:text-light default-font"};function c(t,l){return a(),r("div",n,[o(t.$slots,"default")])}const f=e(s,[["render",c]]);export{f as default};
