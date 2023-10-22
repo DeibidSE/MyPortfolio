675ec2554a5cf54f3af8283263e474a7434883bc
DeibidSE <davidsimon98@outlook.com>
2023-10-22 18:17:36 +0200

    Updates

--- assets/default.37ec5832.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,q as r,Q as o,m as a}from"./entry.e78e65b0.js";const s={},n={class:"bg-gradient-to-r from-purple-600 to-pink-500 text-dark dark:text-light default-font"};function c(t,d){return a(),r("div",n,[o(t.$slots,"default")])}const f=e(s,[["render",c]]);export{f as default};
